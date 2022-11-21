616f70dd9f0c7a7c72a788be7c3781acc71045c5
Maciej <maprz2017@gmail.com>
2022-11-22 00:31:38 +0100

    Added basic grid structure

--- game/game.js
+++ game/game.js
@@ -1,6 +1,10 @@
 
-
+var x = true
 // Game loop
-while(true) {
+while(x == true) {
     // Game actions during playthrough
+    console.log('Game loop start')
+    x = false
+    console.log('Game loop end')
 }
+console.log('Game over')
