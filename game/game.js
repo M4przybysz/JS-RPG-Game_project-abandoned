2858b53427233e2e81735919f7cf4a5b689cfdc1
Maciej <81009361+M4przybysz@users.noreply.github.com>
2022-11-22 10:23:33 +0100

    Update game.js
    Added basic key press detection. (WSAD and Escape)

--- game/game.js
+++ game/game.js
@@ -1,10 +1,21 @@
-
 var x = true
-// Game loop
-while(x == true) {
-    // Game actions during playthrough
-    console.log('Game loop start')
-    x = false
-    console.log('Game loop end')
+var game_grid = document.getElementById('game_grid')
+
+function movement(event) {
+    let player_node = document.getElementById('player_node')
+
+    if(event.keyCode == 87) player_node.textContent = 'up'      // pressed W
+    if(event.keyCode == 83) player_node.textContent = 'down'    // pressed S
+    if(event.keyCode == 65) player_node.textContent = 'left'    // pressed A
+    if(event.keyCode == 68) player_node.textContent = 'right'   // pressed D
+    if(event.keyCode == 27) x = false                           // pressed Escape
+    console.log(event.keyCode, player_node.textContent)
+
+    //event.preventDefault()
 }
-console.log('Game over')
+
+
+// Game loop
+// Game actions during playthrough
+document.addEventListener('keydown', movement)
+
