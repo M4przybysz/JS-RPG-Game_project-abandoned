28bede3678d0f669c83688b14208d52bc57710fa
Maciej <maprz2017@gmail.com>
2022-11-23 22:41:51 +0100

    Added base for stopping the game by pressing Esc

--- game/game.js
+++ game/game.js
@@ -10,22 +10,28 @@ const KEY_TO_ACTION = {
 
 function movement(event) {
     let player_node = document.getElementById('player_node')
+    let stop = document.getElementById('game_stop')
     let key = event.key
 
-    if(key == 'w') {               // pressed W
+    if(key == 'w') {                // pressed W
         player_node.innerHTML = `<img src="assets/test_arrows/arrow_${KEY_TO_ACTION[key]}.png" alt="Sorry. There is no arrow.">` 
     }
-    if(key == 's') {               // pressed S
+    if(key == 's') {                // pressed S
         player_node.innerHTML = `<img src="assets/test_arrows/arrow_${KEY_TO_ACTION[key]}.png" alt="Sorry. There is no arrow.">`
     }   
-    if(key == 'a') {               // pressed A
+    if(key == 'a') {                // pressed A
         player_node.innerHTML = `<img src="assets/test_arrows/arrow_${KEY_TO_ACTION[key]}.png" alt="Sorry. There is no arrow.">`
     }   
-    if(key == 'd') {               // pressed D
+    if(key == 'd') {                // pressed D
         player_node.innerHTML = `<img src="assets/test_arrows/arrow_${KEY_TO_ACTION[key]}.png" alt="Sorry. There is no arrow.">`
     }   
-    if(key == 'escape') {          // pressed Escape
-        x = false  
+    if(key == 'Escape') {           // pressed Escape
+        if(stop.style.display == 'none') {
+            stop.style.display = 'block'  
+        }
+        else {
+            stop.style.display = 'none'
+        }
     }     
 
     console.log(key, player_node.innerHTML)
@@ -37,4 +43,5 @@ function movement(event) {
 // Game loop
 // Game actions during playthrough
 document.addEventListener('keydown', movement)
+document.dispatchEvent(new KeyboardEvent('keydown', {'key': 'Escape'}))
 
