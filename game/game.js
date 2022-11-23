28a35b8a8cd2a999bf1137d5a42830add6b7e32d
Maciej <maprz2017@gmail.com>
2022-11-23 22:15:38 +0100

    Updated keyboard key detection

--- game/game.js
+++ game/game.js
@@ -2,33 +2,41 @@ var x = true
 var game_grid = document.getElementById('game_grid')
 
 const CODE_TO_KEY = {
-    27 : 'escape',
-    65 : 'left',
-    68 : 'right',
-    83 : 'down',
-    87 : 'up'
+    27 : 'ESCAPE',
+    65 : 'A',
+    68 : 'D',
+    83 : 'S',
+    87 : 'W'
+}
+
+const KEY_TO_ACTION = {
+    'A' : 'left',
+    'D' : 'right',
+    'S' : 'down',
+    'W' : 'up'
 }
 
 function movement(event) {
     let player_node = document.getElementById('player_node')
+    let key = event.key.toUpperCase()
 
-    if(event.keyCode == 87) {               // pressed W
-        player_node.innerHTML = `<img src="assets/test_arrows/arrow_${CODE_TO_KEY[event.keyCode]}.png" alt="Sorry. There is no arrow.">` 
+    if(key == 'W') {               // pressed W
+        player_node.innerHTML = `<img src="assets/test_arrows/arrow_${KEY_TO_ACTION[key]}.png" alt="Sorry. There is no arrow.">` 
     }
-    if(event.keyCode == 83) {               // pressed S
-        player_node.innerHTML = `<img src="assets/test_arrows/arrow_${CODE_TO_KEY[event.keyCode]}.png" alt="Sorry. There is no arrow.">`
+    if(key == 'S') {               // pressed S
+        player_node.innerHTML = `<img src="assets/test_arrows/arrow_${KEY_TO_ACTION[key]}.png" alt="Sorry. There is no arrow.">`
     }   
-    if(event.keyCode == 65) {               // pressed A
-        player_node.innerHTML = `<img src="assets/test_arrows/arrow_${CODE_TO_KEY[event.keyCode]}.png" alt="Sorry. There is no arrow.">`
+    if(key == 'A') {               // pressed A
+        player_node.innerHTML = `<img src="assets/test_arrows/arrow_${KEY_TO_ACTION[key]}.png" alt="Sorry. There is no arrow.">`
     }   
-    if(event.keyCode == 68) {               // pressed D
-        player_node.innerHTML = `<img src="assets/test_arrows/arrow_${CODE_TO_KEY[event.keyCode]}.png" alt="Sorry. There is no arrow.">`
+    if(key == 'D') {               // pressed D
+        player_node.innerHTML = `<img src="assets/test_arrows/arrow_${KEY_TO_ACTION[key]}.png" alt="Sorry. There is no arrow.">`
     }   
-    if(event.keyCode == 27) {               // pressed Escape
+    if(key == 'ESCAPE') {          // pressed Escape
         x = false  
     }     
 
-    console.log(event.keyCode, player_node.textContent)
+    console.log(key, player_node.innerHTML)
 
     event.preventDefault()
 }
