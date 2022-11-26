f02a4a892f7b26a57be25d6e02f61b26fd113a04
Maciej <maprz2017@gmail.com>
2022-11-26 16:01:13 +0100

    ticks*

--- game/js/game.js
+++ game/js/game.js
@@ -11,7 +11,7 @@ let start_time = Date.now()
 document.addEventListener('keydown', keydownActions)
 
 //? First Esc press does not activate its action when game_stop display is set to 'none' so I force Esc press
-//? document.dispatchEvent(new KeyboardEvent('keydown', {'key': 'Escape'}))
+// document.dispatchEvent(new KeyboardEvent('keydown', {'key': 'Escape'}))
 
 setInterval(()=>{
     let game_grid = document.getElementById('game_grid')
@@ -23,7 +23,6 @@ setInterval(()=>{
     let current_time = Date.now()
 
     if((current_time - start_time) % 200 == 0) {
-
         if(player_node != null && active_wsad_key != null) {
             player_node.innerHTML = `<img src="assets/test_arrows/arrow_${KEY_MAP[active_wsad_key]}.png" alt="Sorry. There is no arrow.">`
             active_wsad_key = null
