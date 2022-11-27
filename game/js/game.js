0735e86ce93ef38753226da611c665707158d8d4
Maciej <maprz2017@gmail.com>
2022-11-27 21:33:54 +0100

    Added player interface elements tags in game.html

--- game/js/game.js
+++ game/js/game.js
@@ -1,6 +1,6 @@
 //* This file runs is a game loop
 
-//TODO: Define global html game components =============================================================================================
+//TODO: Define global html game components ======================================================================================
 var game_grid = Grid.grid
 var player_node = null
 if(game_grid.innerHTML != '') { 
@@ -25,11 +25,11 @@ function pauseOrUnpauseGame(hardpause) {
 }
 
 // Game ticks handler 
-var interval = 300; // Interval in milliseconds
+var interval = 250; // Interval in milliseconds
 var expected_time_diff = Date.now() + interval; // Expected time difference between interval and now in milliseconds
 var time_diff
 
-function gameTicks() {
+function gameGridTicks() {
     time_diff = Date.now() - expected_time_diff; // Get actual time difference
 
     if(time_diff > interval || document.visibilityState === 'hidden') {return -1} // Time error handling. Errors occur when game tab is not visible to the user
@@ -40,7 +40,7 @@ function gameTicks() {
 
     // Start next loop ==================================
     expected_time_diff += interval;
-    setTimeout(gameTicks, interval - time_diff);
+    setTimeout(gameGridTicks, interval - time_diff);
 
     return 1
 }
@@ -50,12 +50,12 @@ document.addEventListener('keydown', keydownActions)
 
 
 document.onvisibilitychange = () => {
-    if(document.visibilityState === 'visible') {
-        expected_time_diff = Date.now() + interval // Restart gameTicks() every time user is back on the site
-        gameTicks()
+    if(document.visibilityState === 'visible') { // Restart gameGridTicks() every time user is back on the site
+        expected_time_diff = Date.now() + interval 
+        gameGridTicks()
     }
     if(document.visibilityState === 'hidden') { // Puse game when user is not on the site
         pauseOrUnpauseGame(true)
     }
 }
-gameTicks() // First game ticks loop start
+gameGridTicks() // First game ticks loop start
