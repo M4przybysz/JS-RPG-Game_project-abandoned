1535b866d9b4bd66d7a2f6d5386b846a6b4aa439
Maciej <maprz2017@gmail.com>
2022-11-30 00:16:56 +0100

    Created basics for map tables

--- game/saves.js
+++ game/saves.js
@@ -1,5 +1,51 @@
-var deafult_save = {
+const Deafult_save = {
     Player : {
-        name : ""
+        name : null,
+        class : null,
+        lvl : 0,
+        exp : 0,
+        hp : 100,
+        ap : 100,
+        location : 'Test1',
+        position_x : 20,
+        position_y : 20,
+    },
+    Locations : {
+        Test1 : {
+            background_map : [  [],
+                                []
+            ],
+            walls_map : [       [],
+                                []  
+            ],
+            items_map : [       [],
+                                []
+            ],
+            creatures_map : [   [],
+                                []  
+            ],
+            blockade_map : [    [],
+                                [] 
+            ]
+        },
+        Test2 : {
+            background_map : [  [],
+                                []
+            ],
+            walls_map : [       [],
+                                []
+            ],
+            items_map : [       [],
+                                []
+            ],
+            creatures_map : [   [],
+                                []
+            ],
+            blockade_map : [    [],
+                                [] 
+            ]
+        }
     }
 }
+
+const Imported_save = {}
