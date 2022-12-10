8c8a903572f0c86f9667f4f1353519141f5f6c9b
Maciej <maprz2017@gmail.com>
2022-12-10 20:06:13 +0100

    Changed ids file from .json to .js

---
+++ map maker/ids.js
@@ -0,0 +1,12 @@
+var IDs = {
+        Collision : {
+            "all" : "udlr",
+            "up" : "u",
+            "down" : "d",
+            "left" : "l",
+            "right" : "r"
+        },
+        Textures : {
+
+        }
+    }
